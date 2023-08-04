b505b4fe5b939fd3130fcf3865a9ef9e25c8c8ac
Temmie099 <124810321+Temmie099@users.noreply.github.com>
2023-08-04 14:12:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function somar(){
+     var somar;
+     var x, y;
+     x= document.getElementById(x).value;
+     y= document.getElementById(y).value;
+     somar=parseInt(x)+parseInt(y);
+     alert(somar);
+}
+function subtrair(){
+    var subtrair;
+    var x, y;
+    x= document.getElementById(x).value;
+    y= document.getElementById(y).value;
+    subtrair=parseInt(x)-parseInt(y);
+    alert(subtrair);
+}
+function multiplicar(){
+    var multiplicar;
+    var x, y;
+    x= document.getElementById(x).value;
+    y= document.getElementById(y).value;
+    multiplicar=parseInt(x)*parseInt(y);
+    alert(multiplicar);
+}
+function dividir(){
+    var dividir;
+    var x, y;
+    x= document.getElementById(x).value;
+    y= document.getElementById(y).value;
+    dividir=parseInt(x)/parseInt(y);
+    alert(dividir);
+}
