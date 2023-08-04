357f10d71d302c9f7080449ff91f6c441d22f0a4
Temmie099 <124810321+Temmie099@users.noreply.github.com>
2023-08-04 16:18:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 function somar(){
      var somar;
-     var x, y;
-     x= document.getElementById(x).value;
-     y= document.getElementById(y).value;
-     somar=parseInt(x)+parseInt(y);
+     var x1, y1;
+     x1= document.getElementById("x").value;
+     y1= document.getElementById("y").value;
+     somar=parseInt(x1)+parseInt(y1);
      alert(somar);
 }
 function subtrair(){
     var subtrair;
-    var x, y;
-    x= document.getElementById(x).value;
-    y= document.getElementById(y).value;
-    subtrair=parseInt(x)-parseInt(y);
+    var x1, y1;
+    x1= document.getElementById("x").value;
+    y1= document.getElementById("y").value;
+    subtrair=parseInt(x1)-parseInt(y1);
     alert(subtrair);
 }
 function multiplicar(){
     var multiplicar;
-    var x, y;
-    x= document.getElementById(x).value;
-    y= document.getElementById(y).value;
-    multiplicar=parseInt(x)*parseInt(y);
+    var x1, y1;
+    x1= document.getElementById("x").value;
+    y1= document.getElementById("y").value;
+    multiplicar=parseInt(x1)*parseInt(y1);
     alert(multiplicar);
 }
 function dividir(){
     var dividir;
-    var x, y;
-    x= document.getElementById(x).value;
-    y= document.getElementById(y).value;
-    dividir=parseInt(x)/parseInt(y);
+    var x1, y1;
+    x1= document.getElementById("x").value;
+    y1= document.getElementById("y").value;
+    dividir=parseInt(x1)/parseInt(y1);
     alert(dividir);
 }
